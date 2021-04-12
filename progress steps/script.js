bf2869d9b3cbcd8f4012c27e4edcb3b751eae5c7
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-12 11:29:00 +0530

    Progress Steps JS functionality added

--- progress steps/script.js
+++ progress steps/script.js
@@ -1,1 +1,50 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
 
+let currentProgress = 1;
+
+next.addEventListener('click', () => {
+	currentProgress++;
+
+	if (currentProgress > circles.length) {
+		currentProgress = circles.length - 1;
+	}
+
+	update();
+});
+
+prev.addEventListener('click', () => {
+	currentProgress--;
+
+	if (currentProgress < 1) {
+		currentProgress = 1;
+	}
+
+	update();
+});
+
+function update() {
+	circles.forEach((circle, idx) => {
+		if (idx < currentProgress) {
+			circle.classList.add('active');
+		} else {
+			circle.classList.remove('active');
+		}
+	});
+
+	const actives = document.querySelectorAll('.active');
+
+	progress.style.width =
+		((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+	if (currentProgress === 1) {
+		prev.disabled = true;
+	} else if (currentProgress === circles.length) {
+		next.disabled = true;
+	} else {
+		prev.disabled = false;
+		next.disabled = false;
+	}
+}
