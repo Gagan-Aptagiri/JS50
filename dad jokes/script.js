025cbd7856e9e884e5be8c229e8c5f4982461c16
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-20 10:37:18 +0530

    Dad jokes works

--- dad jokes/script.js
+++ dad jokes/script.js
@@ -1,1 +1,22 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
+getJokes();
+
+jokeBtn.addEventListener('click', () => {
+	getJokes();
+});
+
+async function getJokes() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+
+	const response = await fetch('https://icanhazdadjoke.com', config);
+
+	const data = await response.json();
+
+	jokeElement.innerHTML = data.joke;
+}
