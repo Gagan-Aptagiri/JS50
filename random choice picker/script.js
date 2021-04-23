82e621447b6292df27a33dd420e073f3e5d40935
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-23 11:46:58 +0530

    Added Functionality to update the contents of the textarea

--- random choice picker/script.js
+++ random choice picker/script.js
@@ -1,1 +1,24 @@
+const textarea = document.getElementById('textarea');
+const tagsElement = document.getElementById('tags');
 
+textarea.focus();
+
+textarea.addEventListener('input', (event) => {
+	createTags(event.target.value);
+});
+
+function createTags(input) {
+	const tags = input
+		.split(',')
+		.filter((tag) => tag.trim() !== '')
+		.map((tag) => tag.trim());
+
+	tagsElement.innerHTML = '';
+
+	tags.forEach((tag) => {
+		const tagElement = document.createElement('span');
+		tagElement.classList.add('tag');
+		tagElement.innerText = tag;
+		tagsElement.appendChild(tagElement);
+	});
+}
