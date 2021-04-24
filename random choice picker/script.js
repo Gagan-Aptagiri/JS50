12c77984ebb8605c8cfa3d9a1e046332e43ebba3
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-24 21:15:17 +0530

    End of random choice picker and init of navigation animation

--- random choice picker/script.js
+++ random choice picker/script.js
@@ -3,8 +3,16 @@ const tagsElement = document.getElementById('tags');
 
 textarea.focus();
 
-textarea.addEventListener('input', (event) => {
+textarea.addEventListener('keyup', (event) => {
 	createTags(event.target.value);
+
+	if (event.key === 'Enter') {
+		setTimeout(() => {
+			event.target.value = '';
+		}, 10);
+
+		randomSelect();
+	}
 });
 
 function createTags(input) {
@@ -21,4 +29,40 @@ function createTags(input) {
 		tagElement.innerText = tag;
 		tagsElement.appendChild(tagElement);
 	});
+}
+
+function randomSelect() {
+	const times = 30;
+
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+
+		highlightTag(randomTag);
+
+		setTimeout(() => {
+			unHighlightTag(randomTag);
+		}, 100);
+	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+
+		setTimeout(() => {
+			const randomTag = pickRandomTag();
+			highlightTag(randomTag);
+		}, 100);
+	}, times * 100);
+}
+
+function pickRandomTag() {
+	const tags = document.querySelectorAll('.tag');
+	return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+	tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+	tag.classList.remove('highlight');
 }
