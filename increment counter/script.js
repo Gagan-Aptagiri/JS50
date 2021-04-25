5fc8ff2203a4eb8ee53d3d1003976674ea8a11a1
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-25 20:04:34 +0530

    Counter works

--- increment counter/script.js
+++ increment counter/script.js
@@ -1,1 +1,20 @@
+const counters = document.querySelectorAll('.counter');
 
+counters.forEach((counter) => {
+	counter.innerText = '0';
+
+	const updateCounter = () => {
+		const target = +counter.getAttribute('data-target');
+		const countsNumber = +counter.innerText;
+
+		const increment = target / 200;
+
+		if (countsNumber < target) {
+			counter.innerText = `${Math.ceil(countsNumber + increment)}`;
+			setTimeout(updateCounter, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+	updateCounter();
+});
