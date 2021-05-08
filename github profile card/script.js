719612f676967d761a052db827e9406d1f032417
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-08 13:36:17 +0530

    GitHub Profile Card works

--- github profile card/script.js
+++ github profile card/script.js
@@ -1,1 +1,84 @@
+const APIURL = 'https://api.github.com/users/';
 
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+async function getUser(userName) {
+	try {
+		const { data } = await axios(APIURL + userName);
+		createUserCard(data);
+		getRepos(userName);
+	} catch (err) {
+		if (err.response.status == 404) {
+			createErrorCard('No profile with that username');
+		}
+	}
+}
+
+async function getRepos(userName) {
+	try {
+		const { data } = await axios(APIURL + userName + '/repos');
+
+		addReposToCard(data);
+	} catch (err) {
+		console.log(err);
+		createErrorCard('Problem fetching Repos');
+	}
+}
+
+function createUserCard(user) {
+	const cardHTML = `<div class="card">
+				<div>
+					<img
+						src="${user.avatar_url}"
+						alt="${user.name}"
+						class="avatar"
+					/>
+				</div>
+				<div class="user-info">
+					<h2>${user.name}</h2>
+          <p>${user.bio}</p>
+					<ul>
+						<li>${user.followers}<strong>Followers</strong></li>
+						<li>${user.following}<strong>Following</strong></li>
+						<li>${user.public_repos}<strong>Repos</strong></li>
+					</ul>
+
+          <div id="repos"></div>
+				</div>
+			</div>`;
+
+	main.innerHTML = cardHTML;
+}
+
+function createErrorCard(message) {
+	const cardHTML = `
+  <div class="card">
+    <h1>${message}</h1>
+  </div>`;
+	main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+	const reposEl = document.getElementById('repos');
+	repos.slice(0, 5).forEach((repo) => {
+		const repoEl = document.createElement('a');
+		repoEl.classList.add('repo');
+		repoEl.href = repo.html_url;
+		repoEl.target = '_blank';
+		repoEl.innerText = repo.name;
+
+		reposEl.appendChild(repoEl);
+	});
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const user = search.value;
+
+	if (user) {
+		getUser(user);
+		search.value = '';
+	}
+});
