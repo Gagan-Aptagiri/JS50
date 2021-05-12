cbd9d00a317ec257f1e330774f8ce64bee7078d6
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-12 21:58:31 +0530

    Good, Cheap, Fast Checkboxes work

--- good cheap fast checkboxes/script.js
+++ good cheap fast checkboxes/script.js
@@ -1,1 +1,24 @@
+const toggles = document.querySelectorAll('.toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
 
+toggles.forEach((toggle) =>
+	toggle.addEventListener('change', (e) => validate(e.target)),
+);
+
+function validate(e) {
+	if (good.checked && cheap.checked && fast.checked) {
+		if (good === e) {
+			fast.checked = false;
+		}
+
+		if (cheap === e) {
+			good.checked = false;
+		}
+
+		if (fast === e) {
+			cheap.checked = false;
+		}
+	}
+}
