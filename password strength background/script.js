a5b94ab4fdea9a7ffaf9cd6ed00deb1044209978
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-19 22:39:36 +0530

    Added blur functionality, Password Strength background works

--- password strength background/script.js
+++ password strength background/script.js
@@ -1,1 +1,10 @@
+const password = document.querySelector('#password');
+const background = document.querySelector('#background');
 
+password.addEventListener('input', (e) => {
+	const input = e.target.value;
+	const inputLength = input.length;
+	const blurValue = 16 - inputLength * 2;
+
+	background.style.filter = `blur(${blurValue}px)`;
+});
