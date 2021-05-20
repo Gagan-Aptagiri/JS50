78de3efa18736eda960038f5d2905746b942facc
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-20 18:00:35 +0530

    3D Background Works

--- 3d background boxes/script.js
+++ 3d background boxes/script.js
@@ -1,1 +1,17 @@
+const boxes = document.querySelector('#boxes');
+const btn = document.querySelector('#btn');
 
+btn.addEventListener('click', () => boxes.classList.toggle('big'));
+
+function createBox() {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement('div');
+			box.classList.add('box');
+			box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+			boxes.appendChild(box);
+		}
+	}
+}
+
+createBox();
